bab4a95408b6af3bbc8faa355538551279bcf32d
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-12-18 19:33:56 +0300

    Commented out the list items

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const title = document.getElementById('main-heading')
 
-const listItem = document.getElementsByClassName('list-items')
+// const listItem = document.getElementsByClassName('list-items')
 
 const listTags = document.getElementsByTagName('li')
