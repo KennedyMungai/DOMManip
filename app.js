a45360a8641a4016bc01c1d2be1910b770fbcbb9
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-12-18 19:36:34 +0300

    Defined the container querySelector

--- app.js
+++ app.js
@@ -3,3 +3,5 @@ const title = document.getElementById('main-heading')
 // const listItem = document.getElementsByClassName('list-items')
 
 const listTags = document.getElementsByTagName('li')
+
+const container = document.querySelector('div')
