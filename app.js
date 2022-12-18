5a28de54ad594e093af98609907010b43c920b75
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-12-18 19:51:02 +0300

    Aplied styling to all the list itens

--- app.js
+++ app.js
@@ -2,4 +2,10 @@ const title = document.querySelector("#main-heading")
 
 title.style.color = 'red'
 
-const listItems = document.querySelectorAll('.list-items')
+// const listItems = document.querySelectorAll('.list-items')
+
+for (let i = 0; i < listItems.length; i++)
+{
+    const listItem = listItems[i];
+    listItem.style.fontSize = '4rem';
+}
