79e242fcf11339328b5fac4594465902158f1a14
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-12-20 12:29:58 +0300

    Added some logic to the event listener added to the first item on the list

--- app.js
+++ app.js
@@ -2,4 +2,11 @@
 document.querySelector('#football').addEventListener('click', function (e)
 {
     console.log("Football is the shit")
+
+    const target = e.target;
+
+    if (target.matches('li'))
+    {
+        target.style.backgroundColor = 'lightgrey'
+    }
 })
