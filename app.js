fc970f6b7d7ba3082ebe0fa1a102af3d61c89662
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-12-20 20:41:57 +0300

    Defined stopwatch functionality inside the stopWatch function

--- app.js
+++ app.js
@@ -4,3 +4,20 @@ const resetBtn = document.querySelector('#resetBtn')
 let seconds = 0
 let minutes = 0
 let hours = 0
+
+function stopWatch()
+{
+    seconds++
+
+    if (seconds / 60 === 1)
+    {
+        seconds = 0
+        minutes++
+
+        if (minutes / 60 === 1)
+        {
+            minutes = 0
+            hours++
+        }
+    }
+}
