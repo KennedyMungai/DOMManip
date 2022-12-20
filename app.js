111171f294cb862443da943f2c4ad9f84d897bff
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-12-20 13:07:17 +0300

    Added some more logic for the event listeners

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
 document.querySelector('#sports').addEventListener('click', function (e)
 {
     console.log(e.target.getAttribute('id') + ' is clicked')
+
+    const target = e.target
+    if (target.matches('li'))
+    {
+        target.style.backgroundColor = 'gray'
+    }
 })
