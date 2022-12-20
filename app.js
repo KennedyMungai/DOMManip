401f476f2e0beac926441b8aa4e21787ace15fee
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-12-20 15:13:18 +0300

    Added a click event listener to the window component that sets display property of the modal item to none

--- app.js
+++ app.js
@@ -11,3 +11,11 @@ closeBtn.addEventListener('click', function ()
 {
     modalContainer.style.display = 'none'
 })
+
+window.addEventListener('click', function (e)
+{
+    if (e.target === modalContainer)
+    {
+        modalContainer.style.display = 'none'
+    }
+})
