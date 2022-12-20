bb66008b173292ff0e5dda0d8d6e7f4a2d4dfd8b
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-12-20 11:28:42 +0300

    Defined the revealContent function

--- app.js
+++ app.js
@@ -1,2 +1,14 @@
 const revealBtn = document.querySelector('.reveal-btn')
 const hiddenContent = document.querySelector('.hidden-content')
+
+function revealContent()
+{
+    if (hiddenContent.classList.contains('reveal-btn'))
+    {
+        hiddenContent.remove('reveal-btn')
+    }
+    else 
+    {
+        hiddenContent.classList.add('reveal-btn')
+    }
+}
