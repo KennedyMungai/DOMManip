36c04a7abd58e1b8ebaa7070bb623210ede48d6c
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-12-20 10:16:25 +0300

    Console logged out the first child element

--- app.js
+++ app.js
@@ -39,9 +39,9 @@
 let ul = document.querySelector('ul')
 
 // console.log(ul.childNodes)
-// console.log(ul.firstChild)
+console.log(ul.firstChildElement)
 // console.log(ul.lastChild)
 
 // ul.childNodes[1].style.color = 'crimson'
 
-console.log(ul.children)
+// console.log(ul.children)
