a24065263817f55829740d7a6633efbf345f9414
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-12-20 12:31:07 +0300

    Added event listeners to the rest of the items on the list

--- app.js
+++ app.js
@@ -10,3 +10,51 @@ document.querySelector('#football').addEventListener('click', function (e)
         target.style.backgroundColor = 'lightgrey'
     }
 })
+
+document.querySelector('#basketball').addEventListener('click', function (e)
+{
+    console.log("Football is the shit")
+
+    const target = e.target;
+
+    if (target.matches('li'))
+    {
+        target.style.backgroundColor = 'lightgrey'
+    }
+})
+
+document.querySelector('#boxing').addEventListener('click', function (e)
+{
+    console.log("Football is the shit")
+
+    const target = e.target;
+
+    if (target.matches('li'))
+    {
+        target.style.backgroundColor = 'lightgrey'
+    }
+})
+
+document.querySelector('#tennis').addEventListener('click', function (e)
+{
+    console.log("Football is the shit")
+
+    const target = e.target;
+
+    if (target.matches('li'))
+    {
+        target.style.backgroundColor = 'lightgrey'
+    }
+})
+
+document.querySelector('#golf').addEventListener('click', function (e)
+{
+    console.log("Football is the shit")
+
+    const target = e.target;
+
+    if (target.matches('li'))
+    {
+        target.style.backgroundColor = 'lightgrey'
+    }
+})
