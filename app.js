cb08b24ed2819b13c24bdd21eae8c8b088beae96
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-12-21 08:57:20 +0300

    Dynamically added the check and trash icons

--- app.js
+++ app.js
@@ -11,4 +11,14 @@ addTask.addEventListener('click', function ()
     let li = document.createElement('li')
     li.innerText = `${inputTask.value}`
     task.appendChild(li)
+
+    let checkButton = document.createElement('button')
+    checkButton.innerHTML = '<i class="fa-solid fa-check"></i>'
+    checkButton.classList.add('checkTask')
+    task.appendChild(checkButton)
+
+    let deleteButton = document.createElement('button')
+    deleteButton.innerHTML = '<i class="fa-solid fa-trash-can"></i>'
+    deleteButton.classList.add('deleteTask')
+    task.appendChild(deleteButton)
 })
